d24422c07372ddbf9e253d14a74b8f21354cc952
28davidrusnac-dotcom <28.david.rusnac@carolinaday.org>
2026-01-08 14:07:22 +0000

    added python

---
+++ hi.py
@@ -0,0 +1,16 @@
+myInteger=10
+
+myFloat=3.14159
+
+firstString="Hello"
+secondString="David"
+
+print(myInteger)
+print(myFloat)
+print(firstString)
+print(secondString)
+
+myInteger= (input("enter a new integer:"))
+firstString=input("Enter a word:")
+
+print("you enterd the number" + myInteger + "and the word" + firstString)
